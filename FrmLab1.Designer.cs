c3cf9e81a53e31a200d66cef5e496ae4281ddd87
Ethan David Martinez <116189717+ethanmartinez23@users.noreply.github.com>
2022-11-25 15:54:54 +0800

    Add files via upload

---
+++ FrmLab1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CreateTextMartinez
+{
+    partial class FrmLab1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(65, 153);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(110, 41);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(34, 34);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(166, 86);
+            this.txtInput.TabIndex = 1;
+            // 
+            // FrmLab1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 227);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "FrmLab1";
+            this.Text = "FrmLab1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
---
+++ FrmRegistration.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace CreateTextMartinez
+{
+    public partial class FrmRegistration : Form
+    {
+        private string _FullName;
+        private int _Age;
+        private long _ContactNo;
+        private long _StudentNo;
+
+        public static string SetFileName;
+        public FrmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        public long StudentNumber(string studNum)
+        {
+            if (Regex.IsMatch(studNum, @"^[0-9]{1,15}$"))
+            {
+                _StudentNo = long.Parse(studNum);
+            }
+            else
+            {
+                throw new FormatException("User shall input only integer");
+            }
+            return _StudentNo;
+        }
+
+        public long ContactNo(string Contact)
+        {
+
+            if (Regex.IsMatch(Contact, @"^[0-9]{10,11}$"))
+            {
+                _ContactNo = long.Parse(Contact);
+            }
+            else
+            {
+                throw new FormatException("User shall input only integer");
+            }
+            return _ContactNo;
+        }
+
+        public string FullName(string LastName, string FirstName, string MiddleInitial)
+        {
+            if (Regex.IsMatch(LastName, @"^[a-zA-Z]+$") && Regex.IsMatch(FirstName, @"^[a-zA-Z]+$") && Regex.IsMatch(MiddleInitial, @"^[a-zA-Z]+$") || Regex.IsMatch(MiddleInitial, @" ^$"))
+            {
+                _FullName = LastName + ", " + FirstName + ", " + MiddleInitial;
+            }
+            else
+            {
+                throw new FormatException("User shall input only string");
+            }
+            return _FullName;
+        }
+
+        public int Age(string age)
+        {
+            if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
+            {
+                _Age = Int32.Parse(age);
+            }
+            else
+            {
+                throw new FormatException("User shall input only integer");
+            }
+            return _Age;
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StudentInformationClass.SetFullName = FullName(txtLastName.Text, txtFirstName.Text, txtMiddleInitial.Text);
+                StudentInformationClass.SetStudentNo = StudentNumber(txtStudentNo.Text);
+                StudentInformationClass.SetProgram = cbPrograms.Text;
+                StudentInformationClass.SetGender = cbGender.Text;
+                StudentInformationClass.SetContactNo = ContactNo(txtContactNo.Text);
+                StudentInformationClass.SetAge = Age(txtAge.Text);
+                StudentInformationClass.SetBirthday = datePickerBirthday.Value.ToString("yyyy-MM-dd");
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show(fe.Message);
+            }
+
+
+            string getStudentNo = txtStudentNo.Text;
+            string getFirstName = txtFirstName.Text;
+            string getLastName = txtLastName.Text;
+            string getMI = txtMiddleInitial.Text;
+            string getAge = txtAge.Text;
+            string getContactNo = txtContactNo.Text;
+            string getBirthday = datePickerBirthday.Text;
+            string getProgram = cbPrograms.Text;
+            string getGender = cbGender.Text;
+
+            string SetFileName = txtStudentNo.Text + ".txt";
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, SetFileName)))
+            {
+                outputFile.WriteLine("Student No: " + getStudentNo);
+                outputFile.WriteLine("Full Name: " + getLastName + ", " + getFirstName +" " + getMI + ".");
+                outputFile.WriteLine("Program: " + getProgram);
+                outputFile.WriteLine("Gender: " + getGender);
+                outputFile.WriteLine("Birthday: " + getBirthday);
+                outputFile.WriteLine("Contact No.: " + getContactNo);
+
+            }
+            this.Close();
+        }
+
+        private void FrmRegistration_Load(object sender, EventArgs e)
+        {
+
+            string[] ListOfProgram = new string[]
+          {
+                "BS Information Technology",
+                "BS Computer Science",
+                "BS Information Systems",
+                "BS in Accountancy",
+                "BS in Hospitatlity Management",
+                "BS in Tourism Management"
+
+          };
+            for (int i = 0; i < 6; i++)
+            {
+                cbPrograms.Items.Add(ListOfProgram[i].ToString());
+            }
+            string[] IdentityCrisis = new string[]
+            {
+                "Male",
+                "Female"
+            };
+            for (int i = 0; i < 2; i++)
+            {
+                cbGender.Items.Add(IdentityCrisis[i].ToString());
+            }
+
+      
+        }
+    }
+}
+
